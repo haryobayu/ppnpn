1866d2540227eb4305eb27af1f8abc6ab298e428
haryobayu <haryobayu.info@gmail.com>
2019-12-05 15:56:55 +0700

    Update Eksekutif dan Pegawai
    Upload Page Eksekutif dan  Pegawai

---
+++ eksekutif/assets/js/by.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+/* Made by Haryo Bayu 05/12/2019 */
+
+$(document).ready(function(){
+  $(".advscr").hide();
+  $(".zc333").click(function(){
+    $(".advscr").toggle();
+  });
+});
